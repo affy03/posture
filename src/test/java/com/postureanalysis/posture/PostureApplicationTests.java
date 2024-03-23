34fa240be3a7b8a8bb669907c8e059691a0d79bd
progra33erJ <3affy.happy7@gmail.com>
2024-03-23 14:32:39 +0900

    プロジェクトのセットアップ

---
+++ src/test/java/com/postureanalysis/posture/PostureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postureanalysis.posture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
